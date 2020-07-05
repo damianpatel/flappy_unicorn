10b5530bd5b24c14471b40c28dfef45de6873d28
damianpatel <damian.patel2001@gmail.com>
2020-07-05 00:25:15 -0400

    flappy unicorn push

---
+++ source_code/flappy.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Flappy
+{
+    partial class flappy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(flappy));
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.labelEnd = new System.Windows.Forms.Label();
+            this.pipeTop2 = new System.Windows.Forms.PictureBox();
+            this.pipeBottom2 = new System.Windows.Forms.PictureBox();
+            this.star1 = new System.Windows.Forms.PictureBox();
+            this.labelStart = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.AccessibleName = "";
+            this.pipeTop.ErrorImage = null;
+            this.pipeTop.Image = ((System.Drawing.Image)(resources.GetObject("pipeTop.Image")));
+            this.pipeTop.Location = new System.Drawing.Point(533, 11);
+            this.pipeTop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(70, 175);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 0;
+            this.pipeTop.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.AccessibleName = "";
+            this.bird.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.bird.ErrorImage = null;
+            this.bird.Image = ((System.Drawing.Image)(resources.GetObject("bird.Image")));
+            this.bird.Location = new System.Drawing.Point(43, 155);
+            this.bird.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(60, 55);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 1;
+            this.bird.TabStop = false;
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.AccessibleName = "";
+            this.pipeBottom.ErrorImage = null;
+            this.pipeBottom.Image = ((System.Drawing.Image)(resources.GetObject("pipeBottom.Image")));
+            this.pipeBottom.Location = new System.Drawing.Point(533, 407);
+            this.pipeBottom.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(70, 192);
+            this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom.TabIndex = 2;
+            this.pipeBottom.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(-13, 575);
+            this.ground.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(733, 130);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 3;
+            this.ground.TabStop = false;
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(216)))), ((int)(((byte)(149)))));
+            this.labelScore.Font = new System.Drawing.Font("Consolas", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.ForeColor = System.Drawing.Color.OrangeRed;
+            this.labelScore.Location = new System.Drawing.Point(12, 631);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(159, 43);
+            this.labelScore.TabIndex = 4;
+            this.labelScore.Text = "Score: ";
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // labelEnd
+            // 
+            this.labelEnd.AutoSize = true;
+            this.labelEnd.Font = new System.Drawing.Font("OCR A Extended", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEnd.ForeColor = System.Drawing.Color.Red;
+            this.labelEnd.Location = new System.Drawing.Point(24, 212);
+            this.labelEnd.Name = "labelEnd";
+            this.labelEnd.Size = new System.Drawing.Size(661, 78);
+            this.labelEnd.TabIndex = 5;
+            this.labelEnd.Text = "Game Over\nClick Anywhere to Try Again!\r\n";
+            this.labelEnd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelEnd.Visible = false;
+            this.labelEnd.MouseClick += new System.Windows.Forms.MouseEventHandler(this.labelEnd_MouseClick);
+            // 
+            // pipeTop2
+            // 
+            this.pipeTop2.AccessibleName = "";
+            this.pipeTop2.ErrorImage = null;
+            this.pipeTop2.Image = ((System.Drawing.Image)(resources.GetObject("pipeTop2.Image")));
+            this.pipeTop2.Location = new System.Drawing.Point(281, 11);
+            this.pipeTop2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pipeTop2.Name = "pipeTop2";
+            this.pipeTop2.Size = new System.Drawing.Size(70, 94);
+            this.pipeTop2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop2.TabIndex = 6;
+            this.pipeTop2.TabStop = false;
+            this.pipeTop2.Visible = false;
+            // 
+            // pipeBottom2
+            // 
+            this.pipeBottom2.AccessibleName = "";
+            this.pipeBottom2.ErrorImage = null;
+            this.pipeBottom2.Image = ((System.Drawing.Image)(resources.GetObject("pipeBottom2.Image")));
+            this.pipeBottom2.Location = new System.Drawing.Point(281, 252);
+            this.pipeBottom2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pipeBottom2.Name = "pipeBottom2";
+            this.pipeBottom2.Size = new System.Drawing.Size(70, 366);
+            this.pipeBottom2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom2.TabIndex = 7;
+            this.pipeBottom2.TabStop = false;
+            this.pipeBottom2.Visible = false;
+            // 
+            // star1
+            // 
+            this.star1.ErrorImage = null;
+            this.star1.Image = ((System.Drawing.Image)(resources.GetObject("star1.Image")));
+            this.star1.Location = new System.Drawing.Point(203, 293);
+            this.star1.Name = "star1";
+            this.star1.Size = new System.Drawing.Size(38, 38);
+            this.star1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.star1.TabIndex = 8;
+            this.star1.TabStop = false;
+            this.star1.Visible = false;
+            // 
+            // labelStart
+            // 
+            this.labelStart.AutoSize = true;
+            this.labelStart.Font = new System.Drawing.Font("OCR A Extended", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStart.ForeColor = System.Drawing.Color.Red;
+            this.labelStart.Location = new System.Drawing.Point(36, 232);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(569, 39);
+            this.labelStart.TabIndex = 9;
+            this.labelStart.Text = "Press Spacebar to Begin!";
+            this.labelStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flappy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(715, 697);
+            this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.pipeTop2);
+            this.Controls.Add(this.labelEnd);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeBottom);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.pipeBottom2);
+            this.Controls.Add(this.star1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "flappy";
+            this.Text = "Flappy Unicorn Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamKeyIsUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.flappy_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox pipeBottom;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label labelEnd;
+        private System.Windows.Forms.PictureBox pipeTop2;
+        private System.Windows.Forms.PictureBox pipeBottom2;
+        private System.Windows.Forms.PictureBox star1;
+        private System.Windows.Forms.Label labelStart;
+    }
+}
+
---
+++ source_code/flappy.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy
+{
+    public partial class flappy : Form
+    {
+        int pipeSpeed = 4;
+        int gravity = 5;
+        int star1Up = 3;
+        int score = 0;           
+        Random randy = new Random();
+
+        public flappy()
+        {          
+            InitializeComponent();            
+        }
+
+        // Stars appear every other point the player scores
+        // Stars will have a random y-coordinate position when appearing
+        private void Star()
+        {
+            star1.Top += star1Up;
+            if (star1.Top < 100 || star1.Top > 300)
+            {
+                star1Up = -star1Up;
+            }
+
+            if (star1.Left < -60)
+            {
+                star1.Left = 700;
+                star1.Visible = true;
+            }
+               
+            // If unicorn touches a star, they player earns a point
+            if (bird.Bounds.IntersectsWith(star1.Bounds) && star1.Visible == true)
+            {
+                star1.Visible = false;
+                score++;
+            }       
+        }
+
+        private bool EndCondition()
+        {
+            // Conditions that would end the game
+            if (pipeBottom2.Visible == true && pipeTop2.Visible == true)
+            {
+                if (bird.Bounds.IntersectsWith(pipeBottom2.Bounds) ||
+                bird.Bounds.IntersectsWith(pipeTop2.Bounds))
+                {
+                    return true;
+                }
+            }
+            if (bird.Bounds.IntersectsWith(ground.Bounds) || bird.Bounds.IntersectsWith(pipeBottom.Bounds) || 
+                bird.Bounds.IntersectsWith(pipeTop.Bounds) || bird.Top <= 0)
+            {
+                return true;
+            }
+            return false;
+        }    
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {           
+            labelScore.Text = "Score: " + score.ToString();
+            bird.Top += gravity;
+            pipeBottom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            pipeBottom2.Left -= pipeSpeed;
+            pipeTop2.Left -= pipeSpeed;
+            star1.Left -= pipeSpeed;
+
+            // Timer checks whether or not stars should appear
+            Star();         
+
+            // Increases difficulty every 3 points by increasing pipespeed by 1 unit
+            if (score != 0 && score % 3 == 0)
+            {
+                pipeBottom2.Visible = true;
+                pipeTop2.Visible = true;
+                pipeSpeed = NextLevel();
+            // After the player scores 3 points, the second set of pipes
+            } 
+            else if (score >= 3)
+            {
+                pipeBottom2.Visible = true;
+                pipeTop2.Visible = true;
+            }
+            else
+            {
+                pipeBottom2.Visible = false;
+                pipeTop2.Visible = false;
+            }
+
+            if (pipeBottom.Left < -60 && pipeTop.Left < -60)
+            {
+                score ++;
+                pipeBottom.Left = randy.Next(725, 750);
+                pipeTop.Left = randy.Next(725, 750);
+                pipeTop.Height = randy.Next(150,200);
+                pipeBottom.Height = randy.Next(210, 350);
+            }
+
+            if (pipeBottom2.Left < -60 && pipeTop2.Left < -60)
+            {
+                if (pipeBottom2.Visible == true && pipeTop2.Visible == true)
+                {
+                    score ++;
+                }
+                pipeBottom2.Left = randy.Next(700, 732);
+                pipeTop2.Left = randy.Next(700, 732);
+                pipeTop2.Height = randy.Next(60, 80);
+                pipeBottom2.Height = randy.Next(250, 320);
+            }
+
+            if (EndCondition()) 
+            {
+                EndGame();                
+            }          
+        }
+
+        private int NextLevel()
+        {
+            // For every multiple of 3, the function returns an integer that is 1 unit greater than the last
+            // The returned integer will be used to define new pipespeed as game progresses            
+            if (score != 0 && score % 3 == 0)
+            {
+                if (pipeSpeed < 10)
+                {
+                    return (score / 3) + 4;
+                }
+            }
+            return pipeSpeed;
+        }
+
+        private void gameKeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (gameTimer.Enabled == false && labelEnd.Visible == false)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    labelStart.Visible = false;
+                    gameTimer.Enabled = true;
+                }
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {                                   
+                gravity = -10;                                             
+            }
+        }
+
+        private void gamKeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 5;
+            }
+        }
+
+        private void EndGame()
+        {
+            gameTimer.Stop();
+            labelScore.Text = "Score: " + score;
+            labelEnd.Visible = true;
+        }
+
+        private void flappy_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (EndCondition())
+                {
+                    // We must reset all components to original positions
+                    labelEnd.Visible = false;
+                    pipeSpeed = 4;
+
+                    star1.Left = 200;
+                    star1.Visible = false;
+
+                    pipeBottom.Left = 650;
+                    pipeBottom.Height = 190;
+                    pipeTop.Left = 650;
+                    pipeTop.Height = 162;
+
+                    pipeBottom2.Visible = false;
+                    pipeTop2.Visible = false;
+                    pipeBottom2.Left = 280;
+                    pipeBottom2.Height = 330;
+                    pipeTop2.Left = 280;
+                    pipeTop2.Height = 90;
+
+                    bird.SetBounds(43, 155, 47, 43);
+                    score = 0;
+                    labelScore.Text = "Score: 0";
+                    gameTimer.Start();
+                }
+            }
+        }
+
+        private void labelEnd_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (EndCondition())
+                {
+                    // We must reset all components to original positions
+                    labelEnd.Visible = false;
+                    pipeSpeed = 4;
+
+                    star1.Left = 200;
+                    star1.Visible = false;
+
+                    pipeBottom.Left = 650;
+                    pipeBottom.Height = 190;
+                    pipeTop.Left = 650;
+                    pipeTop.Height = 162;
+
+                    pipeBottom2.Visible = false;
+                    pipeTop2.Visible = false;
+                    pipeBottom2.Left = 280;
+                    pipeBottom2.Height = 330;
+                    pipeTop2.Left = 280;
+                    pipeTop2.Height = 90;
+
+                    bird.SetBounds(43, 155, 47, 43);
+                    score = 0;
+                    labelScore.Text = "Score: 0";
+                    gameTimer.Start();
+                }
+            }
+        }
+    }
+}
